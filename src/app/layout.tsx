4e278423b761752a3d566d6078ee46ad2f055ba3
TasosTilsi <tasostilsi@hotmail.com>
2025-05-19 17:31:01 +0300

    ganltcs and gitignore updts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-
 import type { Metadata, Viewport } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import portfolioData from '@/data/portfolio-main-data.json';
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -96,6 +96,18 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(personSchema) }}
         />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-TLWL6FDZE7"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TLWL6FDZE7');
+          `}
+        </Script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} font-mono antialiased h-full flex flex-col`}>
         {children}
